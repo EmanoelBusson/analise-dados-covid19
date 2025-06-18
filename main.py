12e9d5d82ef0f5cb6b6576a3efd56d361dd829ca
EmanoelBusson <emanoelbusson17@gmail.com>
2025-06-17 23:28:04 -0300

    fazendo mudanças no código para fazer o frontend do nosso app usando o streamlit

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/refs/heads/m
 df = df.rename(columns={'newDeaths': 'Novos óbitos', 'newCases': 'Novos casos', 'deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes', 'totalCases_per_100k_inhabitants': 'Casos por 100 mil habitantes'})
 
 # Seleção do Estado
-state = 'MA'
 estados = list(df['state'].unique())
+state = st.sidebar.selectbox('Qual estado?', estados)
 
 # Seleção da coluna
-column = 'Casos por 100 mil habitantes'
+#column = 'Casos por 100 mil habitantes'
 colunas = ['Novos óbitos', 'Novos casos', 'Óbitos por 100 mil habitantes', 'Casos por 100 mil habitantes']
+column = st.sidebar.selectbox('Qual estado?', colunas)
 
 # Seleção das linhas que pertencem ao estado
 df = df[df['state'] == state]
@@ -22,8 +23,9 @@ df = df[df['state'] == state]
 fig = px.line(df, x='date', y=column, title=column + ' - ' + state)
 fig.update_layout(xaxis_title='Data', yaxis_title=column.upper(), title={'x': 0.5})
 
-print('DADOS COVID19 - BRASIL')
-print('Nessa aplicação o usuário pode selecionar o estado e o tipo de informação para ser exibida.')
+st.title('DADOS COVID19 - BRASIL')
+st.write('Nessa aplicação o usuário pode selecionar o estado e o tipo de informação para ser exibida.')
 
-fig.show()
-print('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
+st.plotly_chart(fig, use_container_width=True)
+
+st.caption('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
